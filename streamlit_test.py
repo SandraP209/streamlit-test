d0ffffd30f5ad3229521a44f35fe5e575b6c4717
SandraP209 <sandra.petri@tamkb.nl>
2025-07-01 14:26:06 +0200

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -2,10 +2,11 @@ import streamlit as st
 
 titel_container = st.container()
 
-
 with titel_container:
-     st.title("Test programma start")
+    st.title("Test programma start")
 
 col_opname = st.columns([4.15, 0.85])
 with col_opname[0]:
     opname_klant = st.button("Programma is klaar")
+    if opname_klant:
+        st.success("Je hebt op de knop gedrukt – programma gestart!")
